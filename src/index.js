756e45faba1d0642483356459a107b586eaf9821
Tatsiana Alsheuskaya <ttttanna@gmail.com>
2020-03-12 21:34:20 +0300

    fix: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (arguments.length != 0 && array.length != 0) {
+    array.sort((a,b) => a-b);
+    return array[0];
+  } else return 0;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (arguments.length != 0 && array.length != 0) {
+    array.sort((a,b) => b-a);
+    return array[0];
+  } else return 0;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (arguments.length != 0 && array.length != 0) {
+    let answer = array.reduce((sum, current) => sum + current, 0)
+    return answer/ array.length;
+  } else return 0;
 }
